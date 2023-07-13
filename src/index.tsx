25f52436314e336a21ddd5ecf2cb5e5a2d254281
Tushar <kumar.tushar816@gmail.com>
2023-07-13 12:26:30 +0530

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+const App = () => {
+ return <h1>Where are you flight</h1>;
+ }
+ReactDOM.render(<App />, document.getElementById('root'));
